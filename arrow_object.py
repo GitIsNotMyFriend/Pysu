910bf361e61bee1412543dfca3596b316bd4c4a2
Shahar <Shahar6460@gmail.com>
2017-05-19 18:50:46 +0300

    Adding arrows! + minor calculations
    Added arrows and calculatation of arrows, working on the ability to
    convert osu charts into game beatmaps
    
    - Scroll speed feature is being worked on
    - Arrows sliding down is being worked on

---
+++ arrow_object.py
@@ -0,0 +1,34 @@
+TOTAL_COL = 4
+WIDTH = 512
+
+
+class arrowObject(object):
+
+    def __init__(self, x, y, time, hit_type, hit_end):
+        self.x = x
+        self.y = y
+        self.time = time
+        self.hit_type = hit_type
+        self.hit_end = hit_end
+
+    def get_column(self):
+        xbase = WIDTH / TOTAL_COL
+        minus = WIDTH / 2 / TOTAL_COL
+        return (self.x + minus) / xbase
+
+    def get_time(self):
+        return self.time
+
+
+def arrow_skin(column):
+    FORMAT = "data/noteskin/Arrownote/%s.png"
+    if column == 1:
+        file_open = "left"
+    if column == 2:
+        file_open = "down"
+    if column == 3:
+        file_open = "up"
+    if column == 4:
+        file_open = "right"
+
+    return FORMAT % file_open
--- main.py
+++ main.py
@@ -7,17 +7,39 @@ import sys
 import settings_parse
 import pygame
 import create_stage
+import arrow_object
+
 
 # Constants
 NAME = "Pysu!"
 VERSION = "0.01-ALPHA"
+SCROLL_SPEED = 29
+# Game arrows
+arrows = []
+arrow_1 = arrow_object.arrowObject(192, 192, 0, 1, 0)
+arrow_2 = arrow_object.arrowObject(92, 192, 234, 1, 0)
+arrow_3 = arrow_object.arrowObject(448, 192, 0, 1, 0)
+arrows.append(arrow_1)
+arrows.append(arrow_2)
+arrows.append(arrow_3)
 
 # Directories of files
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'data'))
 beatmaps = os.path.abspath(os.path.join(os.path.dirname(__file__), 'beatmaps'))
 
 # Create dir if doesn't exist (used to generate default settings)
-if not os.path.isdir(basedir): os.mkdir(basedir)
+if not os.path.isdir(basedir):
+    os.mkdir(basedir)
+
+
+def draw_arrows(arrows):
+    arrow_images = []
+    for arrow in arrows:
+        image_dir = arrow_object.arrow_skin(arrow.get_column())
+        arrow_image = pygame.image.load(image_dir)
+        arrow_images.append(arrow_image)
+    return arrow_images
+
 
 # Load normal key images and create image list
 def loadKeyImages():
@@ -28,6 +50,7 @@ def loadKeyImages():
         key_image = pygame.image.load(file)
         image.append(key_image)
     return image
+
 
 # Main game init and loop
 def main():
@@ -53,12 +76,14 @@ def main():
         game_display = pygame.display.set_mode((int(settings["width"]),int(settings["height"])), pygame.NOFRAME)
     pygame.display.set_caption(NAME + " - " + VERSION)
     infoObject = pygame.display.Info()
-
+    i = 0
     # Game run loop
     while 1:
         # Get key images list
         keyImages = loadKeyImages()
+        arrow_images = draw_arrows(arrows)
 
+        original_height, original_width, ratio = 0, 0, 0
         # Check if players wants to leave game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -79,10 +104,17 @@ def main():
         for i, image in enumerate(keyImages):
             original_width, original_height = image.get_size()
             ratio = float(infoObject.current_h) / original_height
-            image = pygame.transform.scale(image, (int(ratio * original_width), int(ratio * original_height)))
+            image = pygame.transform.smoothscale(image, (int(ratio * original_width), int(ratio * original_height)))
             game_display.blit(image, (infoObject.current_w / 2 - original_width * ratio * 4 / 2 + i * original_width * ratio, -50))
 
+        print arrow_images
+        for i, arrow in enumerate(arrow_images):
+            width, height = arrow.get_size()
+            arrow = pygame.transform.smoothscale(arrow, (int(width * ratio * 0.8), int(height * ratio * 0.8)))
+            game_display.blit(arrow, (5 + infoObject.current_w / 2 - original_width * 2 + original_width * ratio * (arrows[i].get_column() - 1), 595 - 192 * arrows[i].get_time() / 1000.0 * SCROLL_SPEED + pygame.time.get_ticks() / 1000.0 * SCROLL_SPEED * 592.0 / 192))
+
         # Update game and tick game
+        print pygame.time.get_ticks()
         pygame.display.update()
         pygame.display.flip()
         clock.tick(144)
